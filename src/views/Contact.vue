c7bf349ad9e276c89da588454ebc573db6895ccb
Nicholas Fisher <avidnick@gmail.com>
2019-08-13 00:11:47 -0400

    First commit

---
+++ src/views/Contact.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="center">
+    <div class="small-text card">To request a demo or find out how you can get Tap Menu in your restaurant, reach us at <a href="mailto:contact@tapmenu.ca" target="_top">contact@tapmenu.ca</a>.</div>
+  </div>
+</template>
+<script>
+export default {
+}
+</script>
+<style lang="scss">
+</style>
